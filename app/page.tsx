0ec8316f4d85f677e369a398d69bc3688faca98c
adairrohling <81537071+adairrohling@users.noreply.github.com>
2023-12-12 22:02:04 +0000

    fix: Configuração Inicial

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+
+    <div>
+      <h1 className='text-center'>Minha Avaliação Substitutiva</h1>
+
+    </div>
+  )
+}
